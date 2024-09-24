4852bbd3c468695943b2d680754d6dee6958b9f5
Karen Fang <karen@dnastack.com>
2024-09-24 14:24:16 -0400

    Fix DGE analysis

--- docker/pydeseq2/scripts/dge_analysis.py
+++ docker/pydeseq2/scripts/dge_analysis.py
@@ -4,8 +4,9 @@ import argparse
 import pandas as pd
 import numpy as np
 import pickle as pkl
-import pydeseq2
-import pytximport
+from pytximport import tximport
+from pydeseq2.dds import DeseqDataSet
+from pydeseq2.ds import DeseqStats
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -15,7 +16,7 @@ def main(args):
     ## METADATA ##
     ##############
     # Remove samples with missing annotations
-    metadata = pd.read_csv(args.metadata)
+    metadata = pd.read_csv(args.metadata, index_col="sample_id")
     samples_to_keep =  ~(metadata.batch.isna() | metadata.condition.isna())
     metadata = metadata.loc[samples_to_keep]
 
@@ -25,31 +26,40 @@ def main(args):
     ############
     new_column_names = ["transcript_id", "gene_id"]
     gene_map = pd.read_csv(args.gene_map, names=new_column_names, header=0)
-    blacklist_genes = pd.read_csv(args.blacklist_genes)
+    blacklist_genes = pd.read_csv(args.blacklist_genes, sep="\t")
 
     path = os.getcwd()
-    samples = metadata["sample_id"].tolist() # TODO change to ASAP_sample_id when DTi has processed metadata
-    files = [os.path.join(path, "/", sample, "_salmon_quant/quant.sf") for sample in samples]
-    #files_dict = dict(zip(samples, files))
+    samples = metadata.index.tolist() # TODO change to ASAP_sample_id when DTi has processed metadata
+    files = [os.path.join(path, f"{sample}_salmon_quant/quant.sf") for sample in samples]
+    files_dict = dict(zip(samples, files))
 
     txi_counts = tximport(
-        files,
-        "salmon",
-        gene_map,
+        file_paths=files,
+        data_type="salmon",
+        transcript_gene_map=gene_map,
     )
+
+    # DESeq2 only takes integers and expects a count df with sample (index) by gene (or AnnData object, txi_counts)
+    counts_int = pd.DataFrame(
+        np.round(txi_counts.X).astype(int),
+        index=txi_counts.obs.index,
+        columns=txi_counts.var.index,
+    )
+    counts_int.index = counts_int.index.map({file_name: sample_name for sample_name, file_name in files_dict.items()})
+
+    # Remove genes with less than 10 read counts total
+    genes_to_keep = counts_int.columns[counts_int.sum(axis=0) >= 10]
+    counts_int = counts_int[genes_to_keep]
 
     # Note: Single factor analysis vs. multifactor analysis requires more manual coding
     dds = DeseqDataSet(
-        counts=txi_counts,
-        clinical=metadata,
+        counts=counts_int,
+        metadata=metadata,
         design_factors=["batch", "condition"],
     )
 
-    # Remove genes with less than 10 read counts total
-    dds.counts = dds.counts.loc[dds.counts.sum(axis=1) >= 10]
-
-    # Remove blacklist genes
-    dds.counts = dds.counts.loc[~dds.counts.index.isin(blacklist_genes)]
+    # TODO - Remove blacklist genes. This file contains regions, not genes. Also contains PD genes.
+    #dds.counts = dds.counts.loc[~dds.counts.index.isin(blacklist_genes)]
 
     # Fit dispersions and LFCs
     dds.deseq2()
@@ -72,6 +82,7 @@ def main(args):
         dds,
         contrast=final_conditions, # Comparing condition only but model uses information from both the condition and batch variables
     )
+    stat_res.summary()
     results_df = stat_res.results_df
     sig_genes = results_df[(results_df["padj"] < padj_threshold) & (results_df["log2FoldChange"].abs() > log2_fc_threshold)]
     sig_genes.to_csv(f"{args.cohort_id}.{args.salmon_mode}.pydeseq2_significant_genes.csv")
@@ -106,7 +117,7 @@ def main(args):
     top_ten_genes = results_df.nsmallest(10, "padj")
     for i, row in top_ten_genes.iterrows():
         plt.annotate(
-            row["gene"],
+            i,
             (row["log2FoldChange"], row["-log10(padj)"]),
             textcoords="offset points",
             xytext=(0,10),
@@ -124,35 +135,35 @@ if __name__ == "__main__":
         description="Differential gene expression analysis using Salmon quantification files with PyDESeq2"
     )
     parser.add_argument(
-        "-c"
+        "-c",
         "--cohort-id",
         type=str,
         required=True,
         help="Cohort ID"
     )
     parser.add_argument(
-        "-m"
+        "-m",
         "--metadata",
         type=str,
         required=True,
         help="Table containing all sample information including batch, condition, etc."
     )
     parser.add_argument(
-        "-g"
+        "-g",
         "--gene-map",
         type=str,
         required=True,
         help="Table containing mapped transcript IDs and gene IDs that must be in this order"
     )
     parser.add_argument(
-        "-b"
+        "-b",
         "--blacklist-genes",
         type=str,
         required=True,
         help="BED file containing the ENCODE Blacklist genes"
     )
     parser.add_argument(
-        "-s"
+        "-s",
         "--salmon-mode",
         type=str,
         required=True,
