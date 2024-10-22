dac7483a8fed6ddf85c3533b73cdb782687d6304
Karen Fang <karen@dnastack.com>
2024-10-22 10:00:20 -0400

    Fix pca plot output name

--- docker/pydeseq2/scripts/cohort_analysis.py
+++ docker/pydeseq2/scripts/cohort_analysis.py
@@ -80,7 +80,7 @@ def main(args):
     plt.ylabel(f"PC2 ({pca.explained_variance_ratio_[1] * 100:.2f}% variance)")
     plt.title("PCA of Normalized Counts by Intervention and Team")
     plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
-    plt.savefig(f"{args.cohort_id}.{args.salmon_mode}.intervention_pca_plot.png", dpi=300, bbox_inches="tight")
+    plt.savefig(f"{args.cohort_id}.{args.salmon_mode}.pca_plot.png", dpi=300, bbox_inches="tight")
     plt.close("all")
 
 
